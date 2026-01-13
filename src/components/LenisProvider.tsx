d3cc468b81ba7c97c0fa5e5fdb91b0058269cc54
Aaysha Khan <aayshakhan8717@gmail.com>
2026-01-13 15:29:02 +0530

    Update LenisProvider.tsx

--- src/components/LenisProvider.tsx
+++ src/components/LenisProvider.tsx
@@ -6,19 +6,26 @@ import { lenisConfig } from "@/config/lenis.config";
 
 export default function LenisProvider({ children }: { children: ReactNode }) {
   const lenisRef = useRef<Lenis | null>(null);
+  const rafId = useRef<number | null>(null);
 
   useEffect(() => {
+   
+    if (lenisRef.current) return;
+
     const lenis = new Lenis(lenisConfig);
     lenisRef.current = lenis;
 
     const raf = (time: number) => {
       lenis.raf(time);
-      requestAnimationFrame(raf);
+      rafId.current = requestAnimationFrame(raf);
     };
 
-    requestAnimationFrame(raf);
+    rafId.current = requestAnimationFrame(raf);
 
     return () => {
+      if (rafId.current) {
+        cancelAnimationFrame(rafId.current);
+      }
       lenis.destroy();
       lenisRef.current = null;
     };
