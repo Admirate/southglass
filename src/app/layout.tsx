63218c23d1d698978c64d49e7cc51f016446a7bd
Aaysha Khan <aayshakhan8717@gmail.com>
2026-01-14 15:09:25 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import CriticalPreload from "@/components/critical-preload";
 import Script from "next/script";
-import LenisProvider from "@/components/LenisProvider";
+import { LenisProvider } from "@/app/providers/LenisProvider";
 
 const inter = Inter({ 
   subsets: ["latin"],
