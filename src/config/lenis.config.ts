8f03df84e8f8fdbdd71a79184603bfeeaa1e022d
Aaysha Khan <aayshakhan8717@gmail.com>
2026-01-15 13:12:57 +0530

    Update lenis.config.ts

--- src/config/lenis.config.ts
+++ src/config/lenis.config.ts
@@ -1,12 +1,8 @@
-import Lenis from "@studio-freight/lenis";
+import type { LenisOptions } from "@studio-freight/lenis";
 
-export const lenisConfig: Lenis.Options = {
-  duration: 1.1, 
+export const lenisConfig: LenisOptions = {
+  duration: 1.1,
   easing: (t: number) => 1 - Math.pow(1 - t, 3),
   smoothWheel: true,
-  smoothTouch: false,
-  wheelMultiplier: 0.85,
-  touchMultiplier: 1,
-  infinite: false,
-  normalizeWheel: false,
 };
+
